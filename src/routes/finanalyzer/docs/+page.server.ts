e9428c9669ae17af71665f461c74d139aa7e4920
John Idogun <sirneij@gmail.com>
2025-01-21 20:36:40 -0500

    Fixed duplicates in categoryDocMappings

--- src/routes/finanalyzer/docs/+page.server.ts
+++ src/routes/finanalyzer/docs/+page.server.ts
@@ -6,13 +6,16 @@ export const load: PageServerLoad = async ({ fetch }) => {
 	const response = await fetch(`${BASE_API_URI}/v1/docs/endpoints`);
 	const docs: ApiDoc[] = await response.json();
 
-	const categoryDocMappings: { category: string; docId: string }[] = [
-		...new Set(
-			docs.map((doc) => {
-				return { category: doc.category, docId: doc._id };
-			})
-		)
-	];
+	const categoryDocMappings = Array.from(
+		docs
+			.reduce((map, doc) => {
+				if (!map.has(doc.category)) {
+					map.set(doc.category, { category: doc.category, docId: doc._id });
+				}
+				return map;
+			}, new Map())
+			.values()
+	);
 	const popularEndpoints = docs.slice(0, 4);
 
 	return {
