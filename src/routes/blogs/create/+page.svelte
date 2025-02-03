dd8e3c1b6dad85960e4e973cb8c5fd994e3a2bf5
John Idogun <sirneij@gmail.com>
2025-02-02 22:54:27 -0500

    Added SEO to the create article page

--- src/routes/blogs/create/+page.svelte
+++ src/routes/blogs/create/+page.svelte
@@ -5,6 +5,13 @@
 	import Dock from '$lib/components/reusables/Dock.svelte';
 </script>
 
+<svelte:head>
+	<title>Create New Article</title>
+	<meta name="description" content="Create a new article on John Owolabi's blog" />
+	<meta name="robots" content="noindex, nofollow" />
+	<meta name="author" content="John Owolabi Idogun" />
+</svelte:head>
+
 <AnimatedContainer>
 	<div class="mx-auto max-w-7xl space-y-8 pt-16">
 		<h1 class="text-center text-3xl font-semibold text-gray-800 dark:text-white">
