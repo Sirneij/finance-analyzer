0b6a32e6f551cf350ca84e740de9499acf7bc4f9
John Idogun <sirneij@gmail.com>
2025-02-11 12:20:50 -0500

    adjust dock bg

--- src/lib/components/reusables/Dock.svelte
+++ src/lib/components/reusables/Dock.svelte
@@ -44,7 +44,7 @@
 			title="Open {title}"
 			in:scale={{ duration: 200, easing: elasticOut }}
 			out:fly={{ x: -300, duration: SLIDE_DURATION, easing: elasticOut }}
-			class="absolute left-0 top-1/2 flex h-10 w-10 -translate-y-1/2 items-center justify-center rounded-r-lg bg-white/90 shadow-lg transition-[transform,background,shadow] duration-300 hover:scale-105 hover:bg-gray-50 hover:shadow-xl dark:bg-gray-800/90 dark:hover:bg-gray-700"
+			class="absolute left-0 top-1/2 flex h-10 w-10 -translate-y-1/2 items-center justify-center rounded-r-lg bg-gray-50 shadow-lg transition-[transform,background,shadow] duration-300 hover:scale-105 hover:bg-gray-100 hover:shadow-xl dark:bg-gray-800 dark:hover:bg-gray-700"
 		>
 			<Collapse
 				class="h-5 w-5 text-gray-900 transition-transform duration-300 
@@ -56,10 +56,7 @@
 		<div
 			in:fly={{ x: -300, duration: SLIDE_DURATION, easing: elasticOut }}
 			out:fly={{ x: -300, duration: SLIDE_DURATION, easing: elasticOut }}
-			class="max-w-64 rounded-r-lg bg-white/90 shadow-xl transition-all duration-300 hover:shadow-2xl lg:bg-transparent dark:bg-gray-900 lg:dark:bg-transparent {page
-				.url.pathname === '/'
-				? 'backdrop-blur-xs'
-				: ''}"
+			class="max-w-64 rounded-r-lg bg-gray-50 shadow-xl transition-all duration-300 hover:shadow-2xl dark:bg-gray-800"
 		>
 			<div class="max-h-[80vh] overflow-y-auto p-4">
 				<div class="mb-4 flex items-center justify-between">
